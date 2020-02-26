6436be45b5c1e6efdb8e2c00dc79f70f2308948d
HOME\bunjac <bunjac92@gmail.com>
2020-02-27 00:17:16 +0100

    uradio prvi domaci

--- lab6-begin/src/main/resources/static/app/js/main.js
+++ lab6-begin/src/main/resources/static/app/js/main.js
@@ -1,5 +1,25 @@
 var wafepaApp = angular.module('wafepaApp', []);
 
 wafepaApp.controller("MyController", function ($scope) {
-    $scope.message = "Hiii";
+    $scope.polje1 = "ovde text";
+    $scope.disableDugme = ($scope.polje1 === "");
+
+    $scope.prazniPolje = function () {
+        $scope.polje1 = "";
+        $scope.disableDugme = true;
+    };
+
+    $scope.kontrolisiDugme = function () {
+        proveriText($scope);
+    };
 });
+
+function proveriText($scope) {
+    if ($scope.polje1 === "") {
+        $scope.disableDugme = true;
+    }
+    else {
+        $scope.disableDugme = false;
+    }
+};
+
