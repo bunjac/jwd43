f819f9e888c319d587696cac5202aa98e9dd7c18
bunjac <javawebdevelopment@u7-10>
2020-03-02 20:56:15 +0100

    end lab 8

--- lab8-begin/src/main/resources/static/app/js/main.js
+++ lab8-begin/src/main/resources/static/app/js/main.js
@@ -1,217 +1,256 @@
-var wafepaApp = angular.module("wafepaApp",["ngRoute"]);
+var wafepaApp = angular.module("wafepaApp", ["ngRoute"]);
 
-wafepaApp.controller("HomeCtrl", function($scope){
-	$scope.message="Hello JWD43!";
+wafepaApp.controller("HomeCtrl", function ($scope) {
+	$scope.message = "Hello JWD43!";
 });
 
-wafepaApp.controller("ActivitiesCtrl", function($scope, $http, $location){
-	
+wafepaApp.controller("ActivitiesCtrl", function ($scope, $http, $location) {
+
 	var url = "/api/activities";
-	
+
 	$scope.activities = [];
-	
-	var getActivities = function(){
+
+	var getActivities = function () {
 		var promise = $http.get(url);
 		promise.then(
-			function success(res){
+			function success(res) {
 				$scope.activities = res.data;
-				//console.log(res);
+				disAlert();
+				// console.log(res);
 			},
-			function error(res){
+			function error(res) {
 				alert("Couldn't fetch activities.");
 			}
 		);
-		//console.log("test");
+		// console.log("test");
 	}
-	
+
 	getActivities();
-	
-	$scope.goToEdit = function(id){
+
+	$scope.goToEdit = function (id) {
 		$location.path("/activities/edit/" + id);
 	}
-	
-	$scope.goToAdd = function(){
+
+	$scope.goToAdd = function () {
 		$location.path("/activities/add");
 	}
+
+	$scope.doDelete = function (id) {
+		$http.delete(url + "/" + id).then(
+			function uspeh(res) {
+				getActivities();
+			},
+			function neuspeh(res) {
+				alert("neuspesno brisanje");
+			}
+		);
+	}
 	
+
 });
 
 
-wafepaApp.controller("EditActivityCtrl", function($scope,$http, $routeParams, $location){
-	//console.log($routeParams);
-	
+wafepaApp.controller("EditActivityCtrl", function ($scope, $http, $routeParams, $location) {
+	// console.log($routeParams);
+
 	var url = "/api/activities/" + $routeParams.aid;
-	
+
 	$scope.activity = {};
 	$scope.activity.name = "";
-	
-	var getActivity = function(){
+
+	var getActivity = function () {
 		var promise = $http.get(url);
 		promise.then(
-			function uspeh(odg){
+			function uspeh(odg) {
 				$scope.activity = odg.data;
 			},
-			function neuspeh(odg){
+			function neuspeh(odg) {
 				alert("Couldn't fetch the activity.");
 			}
 		);
 	}
-	
+
 	getActivity();
-	
-	$scope.doEdit = function(){
+
+	$scope.doEdit = function () {
 		var promise = $http.put(url, $scope.activity);
 		promise.then(
-			function success(res){
+			function success(res) {
 				$location.path("/activities");
 			},
-			function error(){
+			function error() {
 				alert("Couldn't save the activity");
 			}
 		);
 	}
-	
+
 });
 
 
-wafepaApp.controller("AddActivityCtrl", function($scope, $http, $location){
-	
+wafepaApp.controller("AddActivityCtrl", function ($scope, $http, $location) {
+
 	$scope.activity = {};
 	$scope.activity.name = "";
-	
+
 	var url = "/api/activities";
-	
-	$scope.doAdd = function(){
+
+	$scope.doAdd = function () {
 		$http.post(url, $scope.activity).then(
-			function success(){
+			function success() {
 				$location.path("/activities");
 			},
-			function error(){
+			function error() {
 				alert("Couldn't save the activity.");
 			}
 		);
 	}
-	
+
 });
 
 
 
 
-wafepaApp.controller("RecordsCtrl", function($scope, $http, $location){
-	
+wafepaApp.controller("RecordsCtrl", function ($scope, $http, $location) {
+
 	var url = "/api/records";
-	
+
 	$scope.records = [];
-	
+	$scope.users = [];
+	$scope.activities = [];
+
 	$scope.newRecord = {};
 	$scope.newRecord.time = "";
 	$scope.newRecord.duration = "";
 	$scope.newRecord.intensity = "";
-	//TODO: dodati obeležja kojim se povezuje sa korisnikom i aktivnošću
-	
-	
-	var getRecords = function(){
+	// TODO: dodati obeležja kojim se povezuje sa korisnikom i aktivnošću
+	$scope.newRecord.userId = "";
+	$scope.newRecord.activityId = "";
+
+	var getRecords = function () {
 		var promise = $http.get(url);
 		promise.then(
-			function success(res){
+			function success(res) {
 				$scope.records = res.data;
 			},
-			function error(){
+			function error() {
 				alert("Couldn't fetch records");
 			}
 		);
 	}
-	
+
 	getRecords();
-	
-	//TODO: Obezbediti prihvat korisnika i aktivnosti
-	
-	
-	
-	
-	$scope.doAdd = function(){
-		
-		$http.post(url, $scope.newRecord).then(
-			function success(res){
-				getRecords();
-	
+
+	// TODO: Obezbediti prihvat korisnika i aktivnosti
+	var getActivities = function () {
+		$http.get("/api/activities").then(
+			function success(res) {
+				$scope.activities = res.data;
 			},
-			function error(){
+			function error() {
+				alert("Couldn't fetch activities");
+			}
+		);
+	}
+
+	getActivities();
+
+	var getUsers = function () {
+		return $http.get("/api/users").then(
+			function success(res) {
+				$scope.users = res.data;
+			},
+			function error() {
+				alert("Couldn't fetch users.");
+			}
+		);
+	}
+
+	getUsers();
+
+	$scope.doAdd = function () {
+
+		$http.post(url, $scope.newRecord).then(
+			function success(res) {
+				getRecords();
+
+			},
+			function error() {
 				alert("Couldn't save the record");
 			}
 		);
 	}
-	
-	$scope.goToEdit = function(id){
+
+	$scope.goToEdit = function (id) {
 		$location.path("/records/edit/" + id);
 	}
 });
 
 
-wafepaApp.controller("EditRecordCtrl", function($scope, $http, $routeParams, $location){
-	
+wafepaApp.controller("EditRecordCtrl", function ($scope, $http, $routeParams, $location) {
+
 	var recordUrl = "/api/records/" + $routeParams.id;
 	var activitiesUrl = "/api/activities";
 	var usersUrl = "/api/users";
-	
+
 	$scope.record = {};
 	$scope.record.time = "";
 	$scope.record.duration = "";
 	$scope.record.intensity = "";
 	$scope.record.userId = "";
-	$scope.record.activityId = "";	
-	
-	
+	$scope.record.activityId = "";
+
+
 	$scope.activities = [];
 	$scope.users = [];
-	
-	
-	
-	var getActivities = function(){
+
+
+
+	var getActivities = function () {
 		$http.get(activitiesUrl).then(
-			function success(res){
+			function success(res) {
 				$scope.activities = res.data;
+				getUsers();
 			},
-			function error(){
+			function error() {
 				alert("Couldn't fetch activities");
 			}
 		);
 	}
-	
-	var getUsers = function(){
+
+	var getUsers = function () {
 		return $http.get(usersUrl).then(
-			function success(res){
+			function success(res) {
 				$scope.users = res.data;
+				getRecord();
 			},
-			function error(){
+			function error() {
 				alert("Couldn't fetch users.");
 			}
 		);
 	}
-	
-	var getRecord = function(){
+
+	var getRecord = function () {
 		$http.get(recordUrl).then(
-			function success(res){
+			function success(res) {
 				$scope.record = res.data;
 			},
-			function error(){
+			function error() {
 				alert("Couldn't fetch record.");
 			}
 		);
 	}
-	
-	//TODO: Obezbediti redosled izvrsavanja!
+
+	// TODO: Obezbediti redosled izvrsavanja!	
 	getActivities();
-	getUsers();
-	getRecord();
-	
-	
-	$scope.doEdit = function(){
+
+
+
+	$scope.doEdit = function () {
 		$http.put(recordUrl, $scope.record).then(
-			function success(){
+			function success() {
 				$location.path("/records");
 			},
-			function error(){
+			function error() {
 				alert("Something went wrong.");
 			}
 		);
@@ -221,26 +260,26 @@ wafepaApp.controller("EditRecordCtrl", function($scope, $http, $routeParams, $lo
 
 
 
-wafepaApp.config(['$routeProvider', function($routeProvider) {
+wafepaApp.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl : '/app/html/home.html',
+			templateUrl: '/app/html/home.html',
 			controller: "HomeCtrl"
 		})
 		.when('/activities', {
-			templateUrl : '/app/html/activities.html'
+			templateUrl: '/app/html/activities.html'
 		})
 		.when('/activities/add', {
-			templateUrl : '/app/html/add-activity.html'
+			templateUrl: '/app/html/add-activity.html'
 		})
 		.when('/activities/edit/:aid', {
-			templateUrl : '/app/html/edit-activity.html'
+			templateUrl: '/app/html/edit-activity.html'
 		})
 		.when('/records', {
-			templateUrl : '/app/html/records.html'
+			templateUrl: '/app/html/records.html'
 		})
 		.when('/records/edit/:id', {
-			templateUrl : '/app/html/edit-record.html'
+			templateUrl: '/app/html/edit-record.html'
 		})
 		.otherwise({
 			redirectTo: '/'
