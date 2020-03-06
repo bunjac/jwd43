8ff904a6cc0eb9b8ea69c6e21fe78f99bf5ce57d
bunjac92 <bunjac92@gmail.com>
2020-03-06 12:51:41 +0100

    init lab10

---
+++ lab10-begin/src/main/java/jwd/wafepa/TestData.java
@@ -0,0 +1,14 @@
+package jwd.wafepa;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestData {
+
+	@PostConstruct
+	public void init() {
+
+	}
+}
---
+++ lab10-begin/src/main/resources/static/app/js/main.js
@@ -0,0 +1,35 @@
+var wafepaApp = angular.module("wafepaApp",["ngRoute"]);
+
+wafepaApp.controller("HomeCtrl", function($scope){
+	$scope.message="Hello JWD43!";
+});
+
+
+
+
+
+wafepaApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '/app/html/home.html',
+			controller: "HomeCtrl"
+		})
+		.when('/activities', {
+			templateUrl : '/app/html/activities.html'
+		})
+		.when('/activities/add', {
+			templateUrl : '/app/html/add-activity.html'
+		})
+		.when('/activities/edit/:aid', {
+			templateUrl : '/app/html/edit-activity.html'
+		})
+		.when('/records', {
+			templateUrl : '/app/html/records.html'
+		})
+		.when('/records/edit/:id', {
+			templateUrl : '/app/html/edit-record.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
